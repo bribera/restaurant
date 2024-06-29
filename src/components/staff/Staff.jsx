0fda7a64618a07c9e7943f7ac76b4ce5ade43d87
bribera <128254883+bribera@users.noreply.github.com>
2024-06-29 12:50:19 +0200

    changement sur la pge staff

--- src/components/staff/Staff.jsx
+++ src/components/staff/Staff.jsx
@@ -1,5 +1,109 @@
 "use client"
+import Image from 'next/image'
 import React, {useState} from 'react'
+import { MdCall } from 'react-icons/md'
+
+
+const online =[
+  // first
+  {
+    nom:"Sandrine",
+    prenom: "AKPO",
+    role: "Serveuse",
+    call: "+223 55457678",
+    lieu: "Chef employés en ligne"
+  },
+  // second
+  {
+    nom:"Sabin",
+    prenom: "CODJO",
+    role: "Cuisiner",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // third
+  {
+    nom:"Esmé",
+    prenom: "ZOGBO",
+    role: "Réceptionneuse",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // four
+  {
+    nom:"Sky",
+    prenom: "ASSOGBA",
+    role: "Réceptionneur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // five   
+  {
+    nom:"Sky",
+    prenom: "ASSOGBA",
+    role: "Delivreur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // six  
+  {
+    nom:"Rain",
+    prenom: "ASSOGBA",
+    role: "Delivreur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+]
+const place =[
+  // first
+  {
+    nom:"Sandrine",
+    prenom: "AKPO",
+    role: "Serveuse",
+    call: "+223 55457678",
+    lieu: "Chef employés en ligne"
+  },
+  // second
+  {
+    nom:"Sabin",
+    prenom: "CODJO",
+    role: "Cuisiner",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // third
+  {
+    nom:"Esmé",
+    prenom: "ZOGBO",
+    role: "Réceptionneuse",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // four
+  {
+    nom:"Sky",
+    prenom: "ASSOGBA",
+    role: "Réceptionneur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // five   
+  {
+    nom:"Sky",
+    prenom: "ASSOGBA",
+    role: "Delivreur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+  // six  
+  {
+    nom:"Rain",
+    prenom: "ASSOGBA",
+    role: "Delivreur",
+    call: "+223 55457678",
+    lieu: "En ligne"
+  },
+]
 
 const Staff = () => {
 
@@ -22,8 +126,30 @@ const Staff = () => {
         </div>
 
         {/* select button */}
-        <div className="py-[50pxx]">
-
+        <div className="py-[100px]">
+          <div className="">
+            <div className="flex items-center gap-6">
+              {/* image  */}
+              <div className="">
+                <Image src="/images/breadchicken.jpg" width={200} height={500} alt='people' className='rounded-full' />
+              </div>
+              <div className="">
+                <p className="">Jean DOSSOU</p>
+                <p className="">Chef de employés</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              {/* lieu */}
+              <div className="">
+                <p className="">Tous</p>
+              </div>
+              {/* tel */}
+              <div className="flex items-center">
+                <span className=""><MdCall /></span>
+                <p className="">+224 555 774 888</p>
+              </div>
+            </div>
+          </div>
         </div>
         {/* employés */}
 
