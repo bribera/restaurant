1906647351b442b9c3d3c437ccd75ef52f5d5d4b
bribera <128254883+bribera@users.noreply.github.com>
2024-07-02 11:19:26 +0200

    restaurant staff add

--- src/components/staff/Staff.jsx
+++ src/components/staff/Staff.jsx
@@ -126,16 +126,65 @@ const Staff = () => {
         </div>
 
         {/* select button */}
-        <div className="py-[100px]">
-          <div className="">
+        <div className="py-[100px] flex flex-col md:flex-row items-center gap-20">
+          {/* first */}
+          <div className="shadow-lg w-[350px] h-[150px] py-6 px-6 flex flex-col gap-y-6 border border-third rounded-[20px]">
+            <div className="flex relative items-center gap-x-6">
+              {/* image  */}
+              <div className="absolute -top-[55px] -left-5 flex-1 ">
+                <Image src="/images/managerChef.png" width={120} height={100} alt='people' className='rounded-[50%] object-fill shadow-md  border-third border' />
+              </div>
+              <div className="flex flex-1 flex-col items-end ">
+                <p className="leading-wide text-[22px] font-semibold ">Jean DOSSOU</p>
+                <p className="leading-wide text-[18px] font-medium text-first/80">Chef de employés</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              {/* lieu */}
+              <div className="">
+                <p className="text-[17px] font-medium  ">Tous</p>
+              </div>
+              {/* tel */}
+              <div className="flex items-center gap-1">
+                <span className="text-first text-[17px]"><MdCall /></span>
+                <p className="text-[17px]">+224 555 774 888</p>
+              </div>
+            </div>
+          </div>
+          {/* second */}
+          <div className="shadow-md w-fit h-fit">
             <div className="flex items-center gap-6">
               {/* image  */}
               <div className="">
-                <Image src="/images/breadchicken.jpg" width={200} height={500} alt='people' className='rounded-full' />
+                <Image src="/images/managerChef.png" width={200} height={500} alt='people' className='rounded-[50%]' />
               </div>
               <div className="">
-                <p className="">Jean DOSSOU</p>
-                <p className="">Chef de employés</p>
+                <p className="leading-wide text-[22px] font-semibold">Jean DOSSOU</p>
+                <p className="leading-wide text-[18px] font-medium">Chef de employés</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              {/* lieu */}
+              <div className="">
+                <p className="">Tous</p>
+              </div>
+              {/* tel */}
+              <div className="flex items-center">
+                <span className=""><MdCall /></span>
+                <p className="">+224 555 774 888</p>
+              </div>
+            </div>
+          </div>
+          {/* third */}
+          <div className="shadow-md w-fit h-fit">
+            <div className="flex items-center gap-6">
+              {/* image  */}
+              <div className="">
+                <Image src="/images/managerChef.png" width={200} height={500} alt='people' className='rounded-[50%]' />
+              </div>
+              <div className="">
+                <p className="leading-wide text-[22px] font-semibold">Jean DOSSOU</p>
+                <p className="leading-wide text-[18px] font-medium">Chef de employés</p>
               </div>
             </div>
             <div className="flex items-center justify-between">
